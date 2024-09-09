145cd2772c1aef9d6d52338488c6d0da2d817ed7
Linh Ngo <vietlink.ngo@gmail.com>
2024-09-09 18:03:57 +0700

    change the client address and port

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const cors= require('cors');
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8000;
 const bodyParser = require('body-parser');
 app.use(cors({
-    origin:"*"
+    origin:"https://ihm-client.fly.dev/"
 }))
 app.use(express.json());
 app.use(bodyParser.json()); // Để xử lý JSON payload từ request
 
 const sql = require('mssql');
-
+const {Pool}= require('pg');
+const postgresConfig= require('./dbConfig');
+const pool= new Pool(postgresConfig)
 const config = {
     user: 'sa',
     password: '123!@#',
@@ -24,7 +26,7 @@ const config = {
 const connectionString= "Server=tcp:eu-az-sql-serv1.database.windows.net,1433;Initial Catalog=ihmproduction;Persist Security Info=False;User ID=vietlink;Password=G00dday!@#;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 // Middleware
 app.use(express.json());
-
+//sqlServer
 // Định nghĩa API
 app.post('/api/questions', async (req, res) => {
     const { code, question, solution, difficulty, answer } = req.body;
@@ -55,45 +57,104 @@ app.post('/api/questions', async (req, res) => {
     // Gửi phản hồi lại cho client
     res.status(201).send({ message: 'Câu hỏi đã được tạo thành công' });
   });
+// app.post('/api/answers',async(req, res)=>{
+//     const {questionCode, answers}= req.body;
+//     try{
+//         await sql.connect(connectionString);
+//         const request= new sql.Request();
+//         request.input('code', sql.NVarChar(50), questionCode);
+//         request.input('answer', sql.NVarChar(sql.MAX), answers);
+//         const result = await request.execute('uspCheckAnswerByQuestionCode');
+//         if(result.recordset.length>0){
+//             res.json(result.recordset);
+//         }else{
+//             res.json('Answer incorrect');
+//         }
+//     }catch (err) {
+//         console.error('Database connection error', err);
+//         res.status(500).send('Server error');
+//     }
+// })
+  
+// app.get('/api/question/:code', async (req, res) => {
+//     try {
+//         // Kết nối database
+//         await sql.connect(connectionString);
+
+//         // Tạo một đối tượng Request mới
+//         const request = new sql.Request();
+
+//         // Thêm tham số cho Stored Procedure
+//         const questionCode = req.params.code;
+//         request.input('QuestionCode', sql.NVarChar(50), questionCode);
+
+//         // Thực hiện gọi Stored Procedure
+//         const result = await request.execute('GetQuestionByCode');
+
+//         // Kiểm tra và trả về kết quả
+//         if (result.recordset.length > 0) {
+//             res.json(result.recordset);
+//         } else {
+//             res.status(404).send('Question not found');
+//         }
+//     } catch (err) {
+//         console.error('Database connection error', err);
+//         res.status(500).send('Server error');
+//     }
+// });
+//postgress
 app.get('/api/question/:code', async (req, res) => {
     try {
         // Kết nối database
-        await sql.connect(connectionString);
-
-        // Tạo một đối tượng Request mới
-        const request = new sql.Request();
-
-        // Thêm tham số cho Stored Procedure
-        const questionCode = req.params.code;
-        request.input('QuestionCode', sql.NVarChar(50), questionCode);
-
-        // Thực hiện gọi Stored Procedure
-        const result = await request.execute('GetQuestionByCode');
-
-        // Kiểm tra và trả về kết quả
-        if (result.recordset.length > 0) {
-            res.json(result.recordset);
-        } else {
-            res.status(404).send('Question not found');
-        }
+        pool.connect(function(err, client, done){
+            if(err){
+                return console.error('Error fetching client from Pool', err);                
+            }
+            const questionCode = req.params.code;
+            const query = `CALL getquestionbycode($1, $2, $3, $4, $5, $6, $7);`;
+            // Tạo một đối tượng Request mới
+            client.query(query, [questionCode, null, null, null, null, null, null], function(err, result){
+                done();
+                if(err){
+                    return console.error('Error running query', err);
+                }
+                // Kiểm tra và trả về kết quả
+                if (result.rows.length > 0 && result.rows[0].question_id != null) {
+                    res.json(result.rows);
+                } else {
+                    res.status(404).send('Question not found');
+                }
+            });                        
+        });
+        
     } catch (err) {
         console.error('Database connection error', err);
         res.status(500).send('Server error');
     }
 });
 app.post('/api/answers',async(req, res)=>{
-    const {questionCode, answers}= req.body;
+    
     try{
-        await sql.connect(connectionString);
-        const request= new sql.Request();
-        request.input('code', sql.NVarChar(50), questionCode);
-        request.input('answer', sql.NVarChar(sql.MAX), answers);
-        const result = await request.execute('uspCheckAnswerByQuestionCode');
-        if(result.recordset.length>0){
-            res.json(result.recordset);
-        }else{
-            res.json('Answer incorrect');
-        }
+        pool.connect(function(err, client, done){
+            if (err){
+                return console.error('Error fetching client from pool', err);
+            }
+            const {questionCode, answers}= req.body;
+            const query = `CALL checkanswerbyquestioncode($1, $2, $3);`;                
+            client.query(query, [questionCode, answers, null], function (err, result){
+                done();
+                if(err){
+                    return console.error('Error running query', err);
+                }
+                if (result.rows.length > 0 && result.rows[0].question_grade!= null) {
+                    res.json(result.rows[0].question_grade);
+                }else{
+                    res.json('Answer incorrect');
+                }
+            });
+        });
+        
+        
     }catch (err) {
         console.error('Database connection error', err);
         res.status(500).send('Server error');

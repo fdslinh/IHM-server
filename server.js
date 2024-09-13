638888074165077bdf15809366c85acb157c1e15
Linh Ngo <vietlink.ngo@gmail.com>
2024-09-13 16:01:55 +0700

    cc

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors= require('cors');
 const app = express();
-const PORT = 8000 || 3001;
+const PORT = 3000 || 3001;
 const bodyParser = require('body-parser');
 // const websocket= require('ws');
 // const wss= new websocket.Server({port:PORT});
